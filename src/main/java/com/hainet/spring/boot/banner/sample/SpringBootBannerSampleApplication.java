2fc2c799bd3b99f15095dc40d4e0f6daa9916e28
hainet <20400616+hainet@users.noreply.github.com>
2018-05-08 22:26:23 +0900

    Add sample of text banner.

---
+++ src/main/java/com/hainet/spring/boot/banner/sample/SpringBootBannerSampleApplication.java
@@ -0,0 +1,12 @@
+package com.hainet.spring.boot.banner.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBannerSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBannerSampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hainet/spring/boot/banner/sample/SpringBootBannerSampleApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hainet.spring.boot.banner.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootBannerSampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
